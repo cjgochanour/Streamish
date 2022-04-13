8599f80342aa71b5b228ee8bb0631b9d616e3faa
cjgochanour <cjgochanour@gmail.com>
2022-04-13 10:28:21 -0500

    added video controller unit testing

---
+++ Streamish/Utils/StringUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace Streamish.Utils
+{
+    public class StringUtils
+    {
+        public static string MakeExciting(string source)
+        {
+            return $"{source.ToUpper()}!!!";
+        }
+    }
+}
