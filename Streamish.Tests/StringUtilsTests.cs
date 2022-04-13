33e0f49ef44836cb35253e7a44d11634af8ffd37
cjgochanour <cjgochanour@gmail.com>
2022-04-13 11:41:03 -0500

    added string utils tests

---
+++ Streamish.Tests/StringUtilsTests.cs
@@ -0,0 +1,21 @@
+﻿using Streamish.Utils;
+using Xunit;
+
+namespace Streamish.Tests
+{
+    public class StringUtilsTests
+    {
+        [Fact]
+        public void MakeExciting_Method_Makes_A_String_Exciting()
+        {
+            // Arrange - Create any variables, objects or resources needed to run the test
+            var testString = "There's a spider on me";
+
+            // Act - Run the code you want to test (a.k.a "System Under Test")
+            var result = StringUtils.MakeExciting(testString);
+
+            // Assert - Verify that the code from the "Act" step did what we expected
+            Assert.Equal("THERE'S A SPIDER ON ME!!!", result);
+        }
+    }
+}
